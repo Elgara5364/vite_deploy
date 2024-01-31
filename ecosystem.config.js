dfaa5140e0e5fb5ff54a0f36d22d3551d4b25e2a
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 18:01:15 +0900

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,11 @@
     {
       name: "vite-deploy",
 
-      script: "start",
+      script: "npx",
 
       interpreter: "none",
 
-      args: "start ",
-      PM2_SERVE_PATH: "./dist",
-      PM2_SERVE_PORT: 5173,
-      PM2_SERVE_SPA: "true",
+      args: "serve -s build -p 5174",
     }
   ];
 }
